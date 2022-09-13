4bc4024b60b89fb3f44e3c144456fb3d943689a3
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-13 16:32:52 +0800

    Update wnv_no_spray.py

--- pages/wnv_no_spray.py
+++ pages/wnv_no_spray.py
@@ -6,32 +6,56 @@ import pickle
 
 # https://plotly.com/python/mapbox-density-heatmaps/
 @st.cache
-def data(): 
+def data_no_spray(): 
     with open('st_no_sprayed.pkl', 'rb') as f:
         df = pickle.load(f)
     return df
+def data_spray():
+    with open('st_sprayed.pkl', 'rb') as f:
+        df = pickle.load(f)
+    return df
 
-df = data().drop_duplicates()
+no_spray_df = data_no_spray().drop_duplicates()
+spray_df = data_spray().drop_duplicates()
 
 st.sidebar.title('🦟 Identifying presence of West Nile Virus per Trap')
 st.sidebar.info('## What if all trap areas were not sprayed?')
 
 #px.set_mapbox_access_token(open('./.gitignore/.mapbox_token.txt').read())
+col1, col2 = st.columns(2) # format 2 columns 
 
-fig = px.density_mapbox(df, 
-                        lat='Latitude', 
-                        lon='Longitude', 
-                        z='WnvPresent', 
-                        animation_frame='Date',
-                        zoom=9,
-                        height=750,
-                        title='Density map of West Nile Virus if all trap area were NOT sprayed'
-                        )
-fig.update_layout(mapbox_style="carto-positron", 
-                  mapbox_center = {"lat": 41.85, "lon": -87.63}
-                  )
+with col1:
+    fig = px.density_mapbox(no_spray_df, 
+                            lat='Latitude', 
+                            lon='Longitude', 
+                            z='WnvPresent', 
+                            animation_frame='Date',
+                            zoom=9,
+                            height=750,
+                            title='Density map of West Nile Virus if all trap area were NOT sprayed'
+                            )
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_center = {"lat": 41.85, "lon": -87.63}
+                      )
 
-fig.update(layout_coloraxis_showscale=False, 
-           ) # removes default color scale on the side
+    fig.update(layout_coloraxis_showscale=False) # removes default color scale on the side
+           
+    st.plotly_chart(fig, use_container_width=True)
+   
+with col2:
+     fig = px.density_mapbox(spray_df, 
+                            lat='Latitude', 
+                            lon='Longitude', 
+                            z='WnvPresent', 
+                            animation_frame='Date',
+                            zoom=9,
+                            height=750,
+                            title='Density map of West Nile Virus if all trap area were ALL sprayed'
+                            )
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_center = {"lat": 41.85, "lon": -87.63}
+                      )
 
-st.plotly_chart(fig, use_container_width=True)
+    fig.update(layout_coloraxis_showscale=False) # removes default color scale on the side
+           
+    st.plotly_chart(fig, use_container_width=True) 
