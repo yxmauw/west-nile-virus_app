3869cb0175c87014ba51e7f7238f0d6b80565e6d
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-13 15:18:52 +0800

    Update wnv_no_spray.py

--- pages/wnv_no_spray.py
+++ pages/wnv_no_spray.py
@@ -31,7 +31,7 @@ fig = go.Figure(go.Densitymapbox(lat=df.Latitude,
                                xaxis=dict(range=[-88.0, -87.5]))
                )
 
-fig.update_traces(colorbar_tickformatstops=[{dtickrange:[9,None]}], selector=dict(type='densitymapbox'))
+#fig.update_traces(colorbar_tickformatstops=[{dtickrange:[9,None]}], selector=dict(type='densitymapbox'))
 #fig = px.scatter_mapbox(df1, 
                         #lat='Latitude', 
                         #lon='Longitude', 
