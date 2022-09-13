00c166b7680d61a2590fba6c9e13046e45fbb00b
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-13 23:30:59 +0800

    Update trial.py

--- pages/trial.py
+++ pages/trial.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 from plotly.subplots import make_subplots
 import pickle
 
