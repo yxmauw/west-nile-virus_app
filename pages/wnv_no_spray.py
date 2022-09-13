e2e47cddfbc03c3847f7568ac4b3065b32e4499e
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-13 13:17:00 +0800

    Update wnv_no_spray.py

--- pages/wnv_no_spray.py
+++ pages/wnv_no_spray.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import pickle
+import plotly.graph_objects as go
 
 # https://plotly.com/python/mapbox-density-heatmaps/
 @st.cache
@@ -35,7 +36,7 @@ fig.update(layout_coloraxis_showscale=False) # removes default color scale on th
 st.plotly_chart(fig, use_container_width=True)
 
 for i in range(len(df2)):
-    fig.add_trace(df2)
+    fig.add_trace(go.Scattergeo(df2, lat='Latitude', lon='Longitude', mode='markers'))
     # Sleep for a moment just for demonstration purposes, so that the new data
     # animates in.
     time.sleep(0.1)
