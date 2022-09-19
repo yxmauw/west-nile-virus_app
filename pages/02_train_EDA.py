6ee933587f8f99ad51363d13101917b8ca5c8c2d
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-19 14:01:03 +0800

    Update 02_train_EDA.py

--- pages/02_train_EDA.py
+++ pages/02_train_EDA.py
@@ -12,31 +12,31 @@ def train_data():
     df.set_index('Date', inplace=True)
     return df
 
-st.container()
-st.header('Barplots showing Top 20 Traps with highest mosquito count in respective years')
-df = train_data()
+with st.container():
+    st.header('Barplots showing Top 20 Traps with highest mosquito count in respective years')
+    df = train_data()
 
-def barplot1():
-    df1 = pd.DataFrame((df.loc['2007'].groupby(['Trap','Species'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
-    df2 = pd.DataFrame((df.loc['2009'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
-    df3 = pd.DataFrame((df.loc['2011'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
-    df4 = pd.DataFrame((df.loc['2013'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
+    def barplot1():
+        df1 = pd.DataFrame((df.loc['2007'].groupby(['Trap','Species'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
+        df2 = pd.DataFrame((df.loc['2009'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
+        df3 = pd.DataFrame((df.loc['2011'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
+        df4 = pd.DataFrame((df.loc['2013'].groupby(['Species','Trap'])['NumMosquitos'].agg('sum')).sort_values(ascending=False)[:20]).reset_index()
 
-    fig, axes = plt.subplots(4, figsize=(15,20))
+        fig, axes = plt.subplots(4, figsize=(15,20))
     
-    sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df1, ax=axes[0])
-    axes[0].set_title('2007 top 20 traps with mosquitos caught', fontsize=13)
-    sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df2, ax=axes[1])
-    axes[1].set_title('2009 top 20 traps with mosquitos caught', fontsize=13)
-    sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df3, ax=axes[2])
-    axes[2].set_title('2011 top 20 traps with mosquitos caught', fontsize=13)
-    sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df4, ax=axes[3])
-    axes[3].set_title('2013 top 20 traps with mosquitos caught', fontsize=13);
-    for ax in axes: # to iterate each subplot legend position
-        ax.legend(loc='upper right') 
-    plt.subplots_adjust(hspace=0.24) # space between subplots
-    return fig
+        sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df1, ax=axes[0])
+        axes[0].set_title('2007 top 20 traps with mosquitos caught', fontsize=13)
+        sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df2, ax=axes[1])
+        axes[1].set_title('2009 top 20 traps with mosquitos caught', fontsize=13)
+        sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df3, ax=axes[2])
+        axes[2].set_title('2011 top 20 traps with mosquitos caught', fontsize=13)
+        sns.barplot(x='Trap', y='NumMosquitos', hue='Species', data=df4, ax=axes[3])
+        axes[3].set_title('2013 top 20 traps with mosquitos caught', fontsize=13);
+        for ax in axes: # to iterate each subplot legend position
+            ax.legend(loc='upper right') 
+        plt.subplots_adjust(hspace=0.24) # space between subplots
+        return fig
     
-st.pyplot(barplot1())
+    st.pyplot(barplot1())
           
-    
+with st.container():
