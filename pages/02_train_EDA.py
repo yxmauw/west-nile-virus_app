ad3ade39b92ffab85f1dfc2831f70368b4e854ec
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-19 15:29:48 +0800

    Update 02_train_EDA.py

--- pages/02_train_EDA.py
+++ pages/02_train_EDA.py
@@ -52,6 +52,6 @@ with scd_container:
                             #color_discrete_sequence=['SteelBlue','Red'],
                   # zoom=9, height=650, opacity=0.3)
     fig = go.Figure(data=go.Scattergeo(lon=df['Longitude'], lat=df['Latitude'],
-                                   mode='markers', marker_color=df['WnvPresent'], marker_symbol=['x','circle']))
-    fig.update_traces(marker={'color':['steelblue','red']}, selector=dict(type='scattergeo'))
+                                   mode='markers', marker_color=df['WnvPresent']))
+    fig.update_traces(marker={'color':['steelblue','red'], symbol=['x','circle']}, selector=dict(type='scattergeo'))
     st.plotly_chart(fig, use_container_width=True)
