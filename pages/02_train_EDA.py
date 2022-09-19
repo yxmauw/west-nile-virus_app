feda25ebeba3ff3ea384c1e8e4fe09a30b627804
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-19 14:42:38 +0800

    Update 02_train_EDA.py

--- pages/02_train_EDA.py
+++ pages/02_train_EDA.py
@@ -48,5 +48,6 @@ with scd_container:
     st.header('Map showing which traps had West Nile Virus detected')
     px.set_mapbox_access_token(open('./.gitignore/.mapbox_token.txt').read())
     fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="WnvPresent",
-                  color_discrete_sequence=['#4682B4', '#FF0000'], zoom=9, height=650, opacity=0.5)
+                            color_discrete_map={'identity':['#4682B4', '#FF0000']},
+                   zoom=9, height=650, opacity=0.5)
     st.plotly_chart(fig, use_container_width=True)
