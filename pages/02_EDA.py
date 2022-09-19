35d37e2467a15e1f1434401f470b63e0f1a145bb
Belle <103557809+yxmauw@users.noreply.github.com>
2022-09-19 13:42:01 +0800

    Update 02_EDA.py

--- pages/02_EDA.py
+++ pages/02_EDA.py
@@ -4,5 +4,11 @@ import pandas as pd
 import numpy as np
 import pickle
 
+@st.cache
+def train_data(): 
+    with open('st_no_sprayed.pkl', 'rb') as f:
+        df = pickle.load(f)
+    return df
+
 st.container()
 st.header(')
